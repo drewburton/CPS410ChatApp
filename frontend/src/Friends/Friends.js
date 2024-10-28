2383b7650ad9aacf298a9acd77351c2dd5a0b89f
DanielNogueraDevelopment <danielnogueradevelopment@gmail.com>
2024-10-28 10:36:27 -0400

    friend test working on frontend

--- frontend/src/Friends/Friends.js
+++ frontend/src/Friends/Friends.js
@@ -1,18 +1,27 @@
 import './Friends.css';
-
+import React, { useEffect, useState } from 'react';
 function Friends() {
-
+  const [friendresult, setfriendresult] = useState('');
 
   function addFriend(friendName){
     //do friend bs...
         const response = fetch('https://cps410chatapp.onrender.com/addfriend', {
-            method: 'GET',
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: {name:friendName}},
         ).then(function(result){
           //stuff
+          if(result.json().result=="Friend Added"){
+            setfriendresult("Friend Added");
+          }if(result.json().result=="You've already added that friend"){
+            setfriendresult("You've already added that friend");
+          }else if(result.json().result=="No friend found"){
+            setfriendresult("No friend found");
+          }else if(result.json().result=='That is you'){
+            setfriendresult("That is you");
+          }
         })};
   
   return (
@@ -20,7 +29,8 @@ function Friends() {
       <header className="Friends-header">
         <p>
           Add friends: <input className="my-input" type="text"></input>
-          <button >Add</button>
+          <p className='friendaddresult'>{friendresult}</p>
+          <button onClick={addFriend}>Add</button>
         </p>
       </header>
     </div>
--- frontend/src/Friends/Friends.test.js
+++ frontend/src/Friends/Friends.test.js
@@ -1,7 +1,7 @@
 import { render, screen, configure, fireEvent,waitFor } from '@testing-library/react';
 import Friends from './Friends';
 import {act} from 'react';
-test('friends', async function() {
+test('addfriend', async function() {
     
   const element = render(<Friends />);
   const container=element.container;
@@ -9,6 +9,25 @@ test('friends', async function() {
 
   fireEvent.change(input, { target: { value: "Arjay lalonde" } });
   fireEvent.click(screen.getByText('Add'))
+  
+  await waitFor(() => {
+    return Promise.race([
+      container.findByText('Friend Added'),
+      container.findByText('No friend found'),
+      container.findByText('That is you'),
+      container.findByText("You've already added that friend"),
+    ]);
+  }, { timeout: 5000 });
+});
+
+
+
+test('getfriends', async function() {
+    
+  const element = render(<Friends />);
+  const container=element.container;
+  //const input = container.getElementsByClassName('my-input')[0];
+  
   expect(true)
   await waitFor(() => {
     return Promise.race([
